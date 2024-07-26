bcedc3ef3436fca954a9a90a53b2eae4c694a6d1
hsj0402 <hsjain0402@gmail.com>
2024-07-26 22:12:51 +0530

    ecommerce

---
+++ backend/index.js
@@ -0,0 +1,50 @@
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+// Database Connection With MongoDB
+mongoose.connect("mongodb+srv://jainhsj2004:Harsh%40123@cluster0.cljnqio.mongodb.net/Ecommerce")
+
+// API Creation
+
+app.get("/", (req, res) => {
+    res.send("Express App is Running")
+})
+
+// Image Storage Engine
+
+const storage = multer.diskStorage({
+  destination: './upload/images',
+  filename: (req, file, cb) => {
+    return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+  }
+})
+const upload = multer({ storage: storage })
+app.post("/upload", upload.single('product'), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `http://localhost:${port}/images/${req.file.filename}`
+  })
+})
+
+
+// Route for Images folder
+app.use('/images', express.static('upload/images'));
+
+
+app.listen(port, (error) => {
+    if (!error) {
+        console.log("Server Running on Port " + port)
+    }
+    else {
+        console.log("Error : " + error)
+    }
+})
